caf818b84b7dd65a5eb28d5a16c92d56ffdfc0bf
git-Contributor <91840689+git-Contributor@users.noreply.github.com>
2025-01-29 14:42:04 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf install -y tmux 
-dnf install -y bitwarden 
+
+dnf install -y elisa-player 
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
